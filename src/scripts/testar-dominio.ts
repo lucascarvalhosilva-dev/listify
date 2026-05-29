636a1767ef964efd1e15be3cd02e1da10d79e954
lucascarvalhosilva-dev <lucascarvalhosilva412@gmail.com>
2026-05-29 07:13:29 -0300

    chore: remover logs de debug e scripts de teste da integração de variações ML

--- src/scripts/testar-dominio.ts
+++
@@ -1,8 +0,0 @@
-import { buscarDominioML } from '../lib/ml/dominio'
-
-async function main() {
-  const resultado = await buscarDominioML('Camiseta Manga Curta Basica Algodao')
-  console.log(resultado)
-}
-
-main()
